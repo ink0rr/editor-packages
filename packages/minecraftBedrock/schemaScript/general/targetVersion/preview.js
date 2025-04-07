ab2b30cdb9e53b410448f428a73360d8a3ca0c12
ink0rr <tomflynnbeltsazar@gmail.com>
2025-04-07 23:10:16 +0700

    Move schemas

--- packages/minecraftBedrock/schemaScript/general/targetVersion/preview.js
+++
@@ -1,9 +0,0 @@
-const config = await getProjectConfig()
-
-return {
-    type: 'custom',
-    generateFile: 'project/targetVersion/preview.json',
-    data: {
-        if: config.targetVersion && config.targetVersion == '1.21.80'
-    }
-}
